cf91ce36f348a85552b0ea48ef5b504f41a79d96
Fehergyorgymark <fgorgy047@gmail.com>
2024-09-26 11:23:31 +0200

    alapok kész

--- szakkorok/src/main.js
+++ szakkorok/src/main.js
@@ -4,6 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+//Icons: css
+import "bootstrap-icons/font/bootstrap-icons.min.css"
+
 const app = createApp(App)
 
 app.use(router)
---
+++ szakkorok/src/views/szakkorok.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container" >
+    <h1>Szakkörök</h1>
+    <div class="row justify-content-center">
+        <!-- táblázat -->
+         <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Név</th>
+                    <th>Osztály</th>
+                    <th>Szakkör</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- Felsorolás a data-binding segítségével -->
+                <tr v-for="item in users" :key="item.id">
+                    <td>{{ item.name }}</td>
+                    <td>{{ item.class }}</td>
+                    <td>{{ item.studygroupID }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            users: [
+                { id: 1, name: 'John Doe', class: '1A', studygroupID: '123' },
+                { id: 2, name: 'Jane Doe', class: '2B', studygroupID: '456' },
+                { id: 3, name: 'Mike Doe', class: '3C', studygroupID: '789' }
+            ]
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
